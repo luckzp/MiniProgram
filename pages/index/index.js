f26735910925cc9ae2d20058819cd7e24513ed31
pengzhang34 <pengzhang34@iflytek.com>
2020-09-01 17:25:51 +0800

    新增小程序DEMO

---
+++ pages/index/index.js
@@ -0,0 +1,66 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+  
+    navData: [{
+        text: '全站'
+      },
+      {
+        text: '科学'
+      },
+      {
+        text: '数码'
+      },
+      {
+        text: '体育'
+      },
+      {
+        text: '时尚'
+      }
+    ],
+    currentTab: 0,
+    navScrollLeft: 0
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+
+  switchNav(event) {
+    var cur = event.currentTarget.dataset.current;
+    //每个tab选项宽度占1/5
+    var singleNavWidth = this.data.windowWidth / 5;
+    //tab选项居中                            
+    this.setData({
+      navScrollLeft: (cur - 2) * singleNavWidth
+    })
+    if (this.data.currentTab == cur) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: cur
+      })
+    }
+  },
+  onLoad: function () {
+
+    var that = this;
+    wx.request({
+      url: 'https://news-at.zhihu.com/api/4/news/latest',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+         that.setData({
+           list: res.data.stories
+         })
+         console.log(res.data)
+      }
+    })
+  }
+})
