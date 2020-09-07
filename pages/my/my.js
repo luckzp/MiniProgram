992f4650c5955a5239e31ff0011b0e20d9ab01f3
pengzhang34 <pengzhang34@iflytek.com>
2020-09-07 14:43:03 +0800

    调整界面

--- pages/my/my.js
+++ pages/my/my.js
@@ -6,14 +6,14 @@ const app = getApp();
 Page({
     data: {
         userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo')
     },
 
     /**
      * 跳转已购书籍页面
      */
-    goMyBooks: function() {
+    goMyBooks: function () {
         wx.navigateTo({
             url: '../logs/logs'
         });
@@ -22,7 +22,7 @@ Page({
     /**
      * 从 globalData 中获取 userInfo
      */
-    getUserInfo: function() {
+    getUserInfo: function () {
         let that = this;
 
         let userInfo = app.globalData.userInfo;
@@ -42,38 +42,38 @@ Page({
 
     onLoad: function () {
         if (app.globalData.userInfo) {
-          this.setData({
-            userInfo: app.globalData.userInfo,
-            hasUserInfo: true
-          })
-        } else if (this.data.canIUse){
-          // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-          // 所以此处加入 callback 以防止这种情况
-          app.userInfoReadyCallback = res => {
             this.setData({
-              userInfo: res.userInfo,
-              hasUserInfo: true
-            })
-          }
-        } else {
-          // 在没有 open-type=getUserInfo 版本的兼容处理
-          wx.getUserInfo({
-            success: res => {
-              app.globalData.userInfo = res.userInfo
-              this.setData({
-                userInfo: res.userInfo,
+                userInfo: app.globalData.userInfo,
                 hasUserInfo: true
-              })
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
             }
-          })
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
         }
-      },
-      getUserInfo: function(e) {
+    },
+    getUserInfo: function (e) {
         console.log(e)
         app.globalData.userInfo = e.detail.userInfo
         this.setData({
-          userInfo: e.detail.userInfo,
-          hasUserInfo: true
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
         })
-      }
+    }
 })
